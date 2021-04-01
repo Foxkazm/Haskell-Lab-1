14c01f624b11bb4afb2f1eb4ca0f350dba1265cd
Foxkazm <mark.richardson@blackburn.edu>
2021-03-31 20:15:58 -0500

    finished

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Haskell-Lab-1
+after lab took about 2 hours
+wed 7:32-8:17
+thurs 7:41-8:32
--- haskellLab01_Richardson.Mark.hs
+++ haskellLab01_Richardson.Mark.hs
@@ -1,9 +1,60 @@
+-- 1. factorial function
 
 factorial n = product [1..n]
 
+-- 2. write the middle function that returns the middle of a list
+
 middle xs= reverse (drop 1 (reverse (drop 1 xs)))
+
+-- 3. a function to find the second to last element in a list
 
 secondToLast xs= head (drop 1 (reverse xs)) 
 
-halfLength xs= div (length xs) 2
+--4.a function that returns half of a list
 
+halfLength xs= div (length xs) 2
+halfOf xs = take (halfLength xs) xs
+
+--5 a function that takes a list and turns it into a palindrome
+
+palindromify xs = xs ++ tail (reverse xs)
+
+--6 a function that takes three lists and appends them all in reverse order
+
+app3 xs ys zs = reverse zs ++ reverse ys ++ reverse xs
+
+--7 a function that removes elements from a list depending on the first element
+
+remByFirst xs = drop (head xs) xs
+
+--8 a function that finds the lowest value and makes a list of the lowest value the highest value number of times
+
+lowList xs = replicate (maximum xs) (minimum xs)
+
+--9 takes an int and a list and repeats the list the int number of
+
+xTimesL x xs = x*(length xs)
+
+reList x xs= take (xTimesL x xs) (cycle xs)
+
+--10 a function that counts by the first number up to the amount of times given
+
+countBy x y= [x,x*2..x*y]
+
+--11 a function the repeats the highest and lowest values of a list
+
+minMaxDiff xs= maximum xs-minimum xs
+
+minMaxList xs = reList (minMaxDiff xs) [minimum xs, maximum xs]
+
+--12 make a function to find the median of a list
+
+findMedians xs =if mod (length xs) 2 >0 
+                            then
+                            take 1 (drop (halfLength xs) xs)
+                            else
+                            take 2 (drop (halfLength xs-1) xs)
+
+findMedDiff xs= (head (reverse (findMedians xs)) - head (findMedians xs))/2
+
+calcMedian xs=head (findMedians xs) + (findMedDiff xs) 
